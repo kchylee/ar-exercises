988b7233ff26eb7eec451235f015fa8e856b0630
Ken <kchylee@gmail.com>
2017-05-09 22:53:16 +0000

    Finished exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Ken", last_name: "Lee", hourly_rate: 60)
+@store1.employees.create(first_name: "Billy", last_name: "Cheung", hourly_rate: 60)
+@store1.employees.create(first_name: "Rohit", last_name: "Moza", hourly_rate: 60)
+@store1.employees.create(first_name: "Jacques", last_name: "Begin", hourly_rate: 60)
+@store1.employees.create(first_name: "Brandon", last_name: "Galea-Andrews", hourly_rate: 60)
+@store2.employees.create(first_name: "Carlos", last_name: "Garate", hourly_rate: 60)
+@store2.employees.create(first_name: "Matt", last_name: "Socha", hourly_rate: 60)
+@store2.employees.create(first_name: "Keawe", last_name: "Wong", hourly_rate: 60)
+@store2.employees.create(first_name: "Andrew", last_name: "Royce", hourly_rate: 60)
+@store2.employees.create(first_name: "Ziad", last_name: "Mohamed", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Insert a store name"
+puts "> "
+s_name = gets.chomp
+
+new_store = Store.create(name: s_name)
+puts new_store.errors[:name]
+puts new_store.errors[:annual_revenue]
